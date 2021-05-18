576a74796e38c06e78d59e7907dc74c95d2dc70e
unknown <stas.b@code-care.pro>
2021-05-18 22:37:38 +0300

    pravki

--- src/components/pages/BuySoft/BuyButtons.js
+++ src/components/pages/BuySoft/BuyButtons.js
@@ -2,16 +2,24 @@ import { makeStyles, TableCell, TableRow } from '@material-ui/core'
 import React from 'react'
 import { ModalConsumer } from '../../layouts/Layout'
 import { v4 } from 'uuid';
+import * as dayjs from 'dayjs';
+import * as isBetween from 'dayjs/plugin/isBetween';
+dayjs.extend(isBetween);
+
 const useHead = makeStyles((theme) => ({
   TableCell: {
     [theme.breakpoints.down('sm')]: {
       padding: theme.spacing(1)
     }
+  },
+  price: {
+    
   }
 }));
 
 export default function buyButtons({ mobileButtons, handleSetColumn }) {
-
+  const discountValid = dayjs().isBetween('2021-05-17', dayjs('2021-07-01'))
+  const discount = discountValid ? 699 : null
   const buyButtons = [
     {
       price: 119,
@@ -48,7 +56,7 @@ export default function buyButtons({ mobileButtons, handleSetColumn }) {
           </ModalConsumer>
         }
         {buyButtons.map((item, index) => (
-          <React.Fragment key={v4()} >
+          <React.Fragment key={index} >
             {!mobileButtons[index + 2].hideMobileColumn &&
               <ModalConsumer  >
                 {({ handleOpenSoftModal }) =>
@@ -56,9 +64,21 @@ export default function buyButtons({ mobileButtons, handleSetColumn }) {
                     onClick={handleOpenSoftModal(item.type)}
                     className={classes.TableCell}
                   >
-                    <div className='footer' >{item.price}
-                      <p className='text' >USD в год</p>
-                      <div className='buy_text' >Купить</div>
+                    <div className='footer' >
+                      {item.type === 'BuySketchUpStudio' && discount ? (
+                        <>
+                          <strike className='price_text'>{item.price}</strike>
+                          {discount}
+                          <p className='text' >USD в год</p>
+                        </>
+                      ) : (
+                        <>
+                          {item.price}
+                          <p className='text' >USD в год</p>
+                          <div className='buy_text' >Купить</div>
+                        </>
+                      )
+                      }
                     </div>
                   </TableCell>
                 }
