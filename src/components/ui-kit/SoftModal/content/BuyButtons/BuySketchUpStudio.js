a19003af738b5701863b893316aa1cac442fc11e
unknown <stas.b@code-care.pro>
2021-05-19 08:24:12 +0300

    modal discount fix

--- src/components/ui-kit/SoftModal/content/BuyButtons/BuySketchUpStudio.js
+++ src/components/ui-kit/SoftModal/content/BuyButtons/BuySketchUpStudio.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import BuySketchUpShop from './BuySketchUpShop'
+import * as dayjs from 'dayjs';
+import * as isBetween from 'dayjs/plugin/isBetween';
+dayjs.extend(isBetween);
 
 export default function BuySketchUpStudio() {
+  const discountValid = dayjs().isBetween('2021-05-17', dayjs('2021-07-01'))
+  const price = discountValid ? 699 : 1199
+
   return (
-    <BuySketchUpShop priceUSD={1199} product='SketchUpStudio' />
+    <BuySketchUpShop priceUSD={price} product='SketchUpStudio' />
   )
 }
